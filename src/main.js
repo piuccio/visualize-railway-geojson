13f83c34026c9acb0ab3e13e95b0a64e9498dcd1
Fabio Crisci <piuccio@gmail.com>
2019-05-31 14:38:24 +0900

    Show something else

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 /* globals L */
-const LINES = '/node_modules/import-railway-stations/output/lines.json';
+const LINES = '/node_modules/add-open-data-railway-lines/output/lines.json';
+const MISSING = '/node_modules/add-open-data-railway-lines/output/missing.json';
+const CONNECT_ENDPOINT = (lineId, odpt) => `http://localhost:3000/connect?lineId=${encodeURIComponent(lineId)}&odpt=${encodeURIComponent(odpt)}`;
 
 window.onload = init;
 
@@ -14,9 +16,8 @@ function init() {
 
 async function loadGeojson(map) {
   const lines = await (await fetch(LINES)).json();
-  const selectors = document.querySelector('#selectors');
-  const list = createListOfLines(lines);
-  selectors.appendChild(list);
+  const missing = await (await fetch(MISSING)).json();
+  render(lines);
   let layer = null;
 
   function changeSelectedLine() {
@@ -38,17 +39,82 @@ async function loadGeojson(map) {
   window.addEventListener('popstate', changeSelectedLine);
   map.invalidateSize();
   changeSelectedLine();
+  setupToggle(lines, missing);
+}
+
+function render(lines) {
+  const list = document.querySelector('.list');
+  list.innerHTML = '';
+  list.appendChild(createListOfLines(lines));
 }
 
 function createListOfLines(lines) {
-  const list = document.createElement('ul');
+  const list = document.createDocumentFragment();
   Object.keys(lines).forEach((lineId) => {
     const option = document.createElement('li');
     const link = document.createElement('a');
     link.href = `#${lineId}`;
-    link.textContent = lines[lineId].name.ja;
+    link.textContent = lines[lineId].name.en || lines[lineId].name.ja;
     option.appendChild(link);
     list.appendChild(option);
   });
   return list;
+}
+
+function setupToggle(lines, missing) {
+  const all = document.querySelector('.btn-all');
+  const odpt = document.querySelector('.btn-odpt');
+  all.addEventListener('click', () => {
+    render(lines);
+  });
+  odpt.addEventListener('click', () => {
+    renderMissing(lines, missing);
+  });
+  renderMissing(lines, missing);
+}
+
+function renderMissing(lines, missing) {
+  const list = document.querySelector('.list');
+  const frag = document.createDocumentFragment();
+  const template = document.querySelector('#link-lines');
+  
+  missing.forEach((line) => {
+    const clone = document.importNode(template.content, true);
+    const name = clone.querySelector('.link-lines-name');
+    const filter = clone.querySelector('.link-lines-filter');
+    const results = clone.querySelector('.link-lines-results');
+    const { en, ja } = line['odpt:railwayTitle'];
+    filter.value = ja;
+    connectSearch(filter, lines, results, line);
+    name.textContent = `${en} - ${ja}`;
+    frag.appendChild(clone);
+  });
+  list.innerHTML = '';
+  list.appendChild(frag);
+}
+
+function connectSearch(filter, lines, results, missing) {
+  const template = document.querySelector('#link-action');
+  filter.addEventListener('input', (evt) => {
+    const frag = document.createDocumentFragment();
+    const search = evt.target.value;
+    Object.entries(lines).filter(
+      ([,line]) => line.name.ja.includes(search)
+    ).forEach(([lineId, line]) => {
+      const clone = document.importNode(template.content, true);
+      clone.querySelector('.link-action-name').textContent = line.name.ja;
+      clone.querySelector('.link-action-btn').addEventListener('click', linkListener(lineId, missing));
+      frag.appendChild(clone);
+    });
+    results.innerHTML = '';
+    results.appendChild(frag);
+  });
+}
+
+function linkListener(lineId, missing) {
+  return async () => {
+    await fetch(CONNECT_ENDPOINT(lineId, missing['@id']), {
+      method: 'PUT',
+    });
+  };
 }
